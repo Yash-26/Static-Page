9479c2d64c96c6005737162b66e95be747c93ba9
Yashwanth <107934128+Yash-26@users.noreply.github.com>
2023-04-22 19:25:20 +0530

    Add files via upload
    updated

---
+++ index.js
@@ -0,0 +1,15 @@
+function yt(){
+  window.open("https://www.youtube.com/");
+ }
+ function netflix(){
+  window.open("https://www.netflix.com/in/");
+ }
+ function hotstar(){
+  window.open("https://www.hotstar.com/in/home?ref=%2Fin");
+ }
+ function prime(){
+  window.open("https://www.primevideo.com/offers/nonprimehomepage/ref=atv_hm_pri_c_9zZ8D2_1_0");
+ }
+ function msg(){
+  alert("Quick!!Register now to get access");
+ }
